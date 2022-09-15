79ceec417c43f5b1a519c471143382b4f7f3cc79
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-09-15 18:17:13 +0100

    Adding things

--- src/donation/donation.service.ts
+++ src/donation/donation.service.ts
@@ -292,6 +292,11 @@ export class DonationService {
         donationId: donation.id
       }
     });
+    await this.prisma.donator.deleteMany({
+      where: {
+        donationId: donation.id
+      }
+    });
     if(!donation) throw new NotFoundException('Doação não encontrada');
     return await this.prisma.donation.delete({
       where: {
